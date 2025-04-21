50f3a3fc837b86be99f67b0f9a36dc10f13a6750
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-04-20 21:46:31 -0500

    T04 vs Thrust done

--- analysis.py
+++ analysis.py
@@ -1,14 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('ECO3.tsv', sep='\t', header=1)
 
 df.columns = df.columns.str.replace(r" \(.+\)", '', regex=True)
 
-df = df[df["RPM"] > 100]
-
-print(df)
-
-import matplotlib.pyplot as plt
+df = df[(df["RPM"] > 100) & (df["RPM"] < df["RPM"].max())]
 
 plt.title("T04 vs Thrust")
 plt.plot(df["Thrust"], df["T4"])
